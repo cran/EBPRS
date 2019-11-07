24ee1d7e4d596ac3f49121d2e490fa9ab42150c4
Shuang Song <song-s19@mails.tsinghua.edu.cn>
2019-11-07 15:10:02 +0000

    version 1.2.2

--- R/agtc.R
+++ R/agtc.R
@@ -1,30 +1,32 @@
 agtc <- function(a,b){
   sig <- rep(1,length(a))
   diff <- which(a!=b)
-  for(i in 1:length(diff)){
-    j=diff[i]
-    if(a[j]=="A"){
-      if(b[j]!="T"){
-        sig[j] <- -1
+  if(length(diff)>0){
+    for(i in 1:length(diff)){
+      j=diff[i]
+      if(a[j]=="A"){
+        if(b[j]!="T"){
+          sig[j] <- -1
+        }
       }
-    }
-    if(a[j]=="T"){
-      if(b[j]!="A"){
-        sig[j] <- -1
-      }
+      if(a[j]=="T"){
+        if(b[j]!="A"){
+          sig[j] <- -1
+        }
 
-    }
-    if(a[j]=="G"){
-      if(b[j]!="C"){
-        sig[j] <- -1
       }
+      if(a[j]=="G"){
+        if(b[j]!="C"){
+          sig[j] <- -1
+        }
 
-    }
-    if(a[j]=="C"){
-      if(b[j]!="G"){
-        sig[j] <- -1
       }
+      if(a[j]=="C"){
+        if(b[j]!="G"){
+          sig[j] <- -1
+        }
 
+      }
     }
   }
   return(sig)
